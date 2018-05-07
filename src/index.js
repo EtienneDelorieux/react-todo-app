a4885b937c1ed97aa38d91872bb65a4da8dd4101
Etienne <etienne.delorieux@gmail.com>
2018-05-07 14:13:19 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+import Login from './components/login';
+
+ReactDOM.render(<Login />, document.getElementById('root'));
